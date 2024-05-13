8ff60cd031a2b512d761b5c59016fdd37a717484
IliaYa <iliapresnya@gmail.com>
2024-05-13 14:39:51 +0300

    dsfsdf

---
+++ main.py
@@ -0,0 +1,34 @@
+#заготовка А
+def Skob()
+    for i in range(n):
+        a = input()
+        n = []
+        n.append(a)
+        if n[-1] == "(":
+            print("no")
+        elif n[-1] == "{":
+            print("no")
+        elif n[-1] == "[":
+            print("no")
+        else:
+            print("yes")
+
+#Заготовка B
+N, x, y = map(int, input().split())
+
+
+# Заготовка C
+def matrix_sum():
+    x, y = map(int, input().split())
+    z = [list(map(int, input().split())) for i in range[x]]
+    q = [[0] * y for i in range[x]]
+    w = [[''] * y for i in range[x]]
+
+# заготовка ООП
+class Arch()
+    def __init__(self):
+
+    def arch(self):
+
+    def anarch(self):
+        readfrom
